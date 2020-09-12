d4d3bdbab680078ca938635d83caa64d5f9f404d
Arthur-Rs <arthurreis074@gmail.com>
2020-09-12 14:16:37 -0300

    Bug Fixed

--- eventsworkshops/script.js
+++ eventsworkshops/script.js
@@ -2,7 +2,7 @@ const calendar = new Calendar(
 	'calendarContainer',
 	'small',
 	[ 'Sábado', 3 ],
-	[ '#292f63', '#2a3166', '#f1f1f1', '#9ba4d3' ],
+	[ '#1e2969', '#1e254d', '#f1f1f1', '#9ba4d3' ],
 	{
 		days: [ 'Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado' ],
 		months: [
